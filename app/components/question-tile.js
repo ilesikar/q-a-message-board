b07bd6c5a8d3bea07ee053caadb9f8686bec2209
Ian Lesikar <ilesikar@gmail.com>
2016-06-10 14:58:39 -0700

    added show/hide functions for notes

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  areNotesShowing: false,
+  actions: {
+    showNotes: function() {
+      this.set('areNotesShowing', true);
+    },
+    hideNotes: function() {
+      this.set('areNotesShowing', false);
+    }
+  }
 });
